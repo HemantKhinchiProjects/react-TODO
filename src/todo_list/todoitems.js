07a8e5928fa9f1e6e97cb4055255a4c3e4e859cf
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-30 15:54:08 +0530

    2 files modified

--- src/todo_list/todoitems.js
+++ src/todo_list/todoitems.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import './todoitems.css';
 const Todolisttems = (props) => {
-  const [title, setTitle] = useState(props.title);
+  const [list, setList] = useState([]);
+  const removeList = (id) => {
+    const newTours = list.filter((item) => item.id !== id);
+    setList(newTours);
+    alert();
+  };
 
-  return <li id={props.id}>{props.value}</li>;
+  return (
+    <li id={props.id}>
+      {props.value}{' '}
+      <button className="delete-btn" onClick={() => removeList(id)}>
+        Complete
+      </button>
+    </li>
+  );
 };
 export default Todolisttems;
